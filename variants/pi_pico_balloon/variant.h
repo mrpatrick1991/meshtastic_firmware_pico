29c525d010d42120d8297a73360f6865470ec5a9
Matthew Patrick <64496719+mrpatrick1991@users.noreply.github.com>
2024-05-09 09:48:14 -0600

    got the pi pico to compile and run on rfm98 chipset

--- variants/pi_pico_balloon/variant.h
+++ variants/pi_pico_balloon/variant.h
@@ -1,53 +1,22 @@
+// #define RADIOLIB_CUSTOM_ARDUINO 1
+// #define RADIOLIB_TONE_UNSUPPORTED 1
+// #define RADIOLIB_SOFTWARE_SERIAL_UNSUPPORTED 1
+
 #define ARDUINO_ARCH_AVR
 
-// Build with slow system clock enabled to reduce power consumption.
-#define RP2040_SLOW_CLOCK
-
-#ifdef RP2040_SLOW_CLOCK
-// Redefine UART1 serial log output to avoid collision with UART0 for GPS.
-#define SERIAL2_TX -1
-#define SERIAL2_RX -1
-// Reroute log output in SensorLib when USB is not available
-#define log_e(...) Serial2.printf(__VA_ARGS__)
-#define log_i(...) Serial2.printf(__VA_ARGS__)
-#define log_d(...) Serial2.printf(__VA_ARGS__)
-#endif
-
-// Traquito board has a GPS
-#define HAS_GPS 1
-
-// Disable OLED Screen
-#define HAS_SCREEN 0
-#define USE_SH1106 0
-#define RESET_OLED -1
-
-// Redefine I2C0 pins to avoid collision with UART1/Serial2.
-#define I2C_SDA -1
-#define I2C_SCL -1
-
-// Redefine Waveshare UPS-A/B I2C_1 pins:
-#define I2C_SDA1 -1
-#define I2C_SCL1 -1
-
-// no INA219
-#undef INA219_MULTIPLIER 
-
-// Traquito ATGM 336H GPS 
-#define GPS_RX_PIN 9
-#define GPS_TX_PIN 8
-
-// Wakeup from backup mode
-// #define PIN_GPS_FORCE_ON 14
-// No GPS reset available
-#undef PIN_GPS_RESET
-#undef PIN_GPS_PPS // no PPS
-
-// Traquito GPS reset must be turned high impedence to enable GPS
-#define PIN_GPS_STANDBY 6
+#define EXT_NOTIFY_OUT -1
+#define BUTTON_PIN -1
 
 #define LED_PIN PIN_LED
 
-#undef USE_SX1262 // RFM98 module is SX127x
+#define BATTERY_SENSE_RESOLUTION_BITS ADC_RESOLUTION
+#define BATTERY_PIN -1
+
+#define GPS_RX_PIN 6
+#define GPS_TX_PIN 7
+
+#undef USE_SX126X
+#define USE_RF95
 
 #undef LORA_SCK
 #undef LORA_MISO
@@ -59,17 +28,7 @@
 #define LORA_MOSI 11
 #define LORA_CS 3
 
-#define LORA_DIO0 RADIOLIB_NC
 #define LORA_RESET 15
-#define LORA_DIO1 20
+#define LORA_DIO0 20
+#define LORA_DIO1 RADIOLIB_NC
 #define LORA_DIO2 2
-#define LORA_DIO3 RADIOLIB_NC
-
-#ifdef USE_SX1262
-#define SX126X_CS LORA_CS
-#define SX126X_DIO1 LORA_DIO1
-#define SX126X_BUSY LORA_DIO2
-#define SX126X_RESET LORA_RESET
-#define SX126X_DIO2_AS_RF_SWITCH
-#define SX126X_DIO3_TCXO_VOLTAGE 1.8
-#endif
